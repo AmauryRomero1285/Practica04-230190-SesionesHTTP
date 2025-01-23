23c8f6afd164d12bd3de0b11f08eaa00dc7d6390
AmauryRomero1285 <230190@utxicotepec.edu.mx>
2025-01-23 09:58:36 -0600

    Practica02
    sesiones

--- app.js
+++ app.js
@@ -17,22 +17,14 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Sesiones almacenadas en memoria (RAM)
-const sessions = {};
 app.use(
   session({
     secret: "P4-YARM#gokusupersaiyajin-SesionesHTTP-VariablesDeSesión",
     resave: false,
     saveUninitialized: false,
-    cookie: { maxAge: 5 * 60 * 1000 },
+    cookie: { maxAge: 5 * 100 * 1000 }, // Tiempo de vida de la cookie de la sesión
   })
 );
-
-app.get("/", (req, res) => {
-  return res.status(200).json({
-    message: "Bienvenid@ a la API de Control de Sesiones",
-    author: "Yared Amaury Romero Martinez",
-  });
-});
 
 // Función de utilidad que nos permitirá acceder a la información de la interfaz de red
 const getLocalIP = () => {
@@ -49,6 +41,13 @@ const getLocalIP = () => {
   return null; // Retorna cuando no se encuentra una IP válida
 };
 
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    message: "Bienvenid@ a la API de Control de Sesiones",
+    author: "Yared Amaury Romero Martinez",
+  });
+});
+
 // Login Endpoint
 app.post("/login", (req, res) => {
   const { email, nickname, macAddress } = req.body;
@@ -58,10 +57,12 @@ app.post("/login", (req, res) => {
       .status(400)
       .json({ message: "Se requiere que los campos estén llenos" });
   }
+
   const sessionId = uuidv4();
   const now = new Date();
 
-  sessions[sessionId] = {
+  // Almacenar la sesión en req.session
+  req.session.user = {
     sessionId,
     email,
     nickname,
@@ -81,48 +82,55 @@ app.post("/login", (req, res) => {
 app.post("/logout", (req, res) => {
   const { sessionId } = req.body;
 
-  if (!sessionId || !sessions[sessionId]) {
+  if (!sessionId || !req.session.user || req.session.user.sessionId !== sessionId) {
     return res
       .status(404)
       .json({ message: "No se ha encontrado una sesión activa" });
   }
 
-  delete sessions[sessionId];
+  // Eliminar la sesión
   req.session.destroy((err) => {
     if (err) {
       return res.status(500).send("Error al cerrar sesión");
     }
+    res.status(200).json({ message: "Logout exitoso" });
   });
-
-  res.status(200).json({ message: "Logout exitoso" });
 });
 
 // Update Endpoint
-app.post("/update", (req, res) => {
+app.put("/update", (req, res) => {
   const { sessionId, email, nickname } = req.body;
 
-  if (!sessionId || !sessions[sessionId]) {
+  if (!req.session.user || req.session.user.sessionId !== sessionId) {
     return res.status(404).json({ message: "No existe una sesión activa" });
   }
 
-  if (email) sessions[sessionId].email = email;
-  if (nickname) sessions[sessionId].nickname = nickname;
+  // Actualizar los campos si es necesario
+  if (email) req.session.user.email = email;
+  if (nickname) req.session.user.nickname = nickname;
 
-  sessions[sessionId].lastAccessedAt = new Date();
+  req.session.user.lastAccessedAt = new Date();
 
   res.status(200).json({
     message: "Actualización exitosa",
+    session: req.session.user,
   });
 });
 
 // Status Endpoint
 app.post("/status", (req, res) => {
   const sessionId = req.query.sessionId;
-  if (!sessionId || !sessions[sessionId]) {
+
+  if (!req.session.user || req.session.user.sessionId !== sessionId) {
     return res.status(404).json({ message: "No hay sesiones activas" });
   }
+
+  const lastAccessedAtFormatted = moment(req.session.user.lastAccessedAt).format('YYYY-MM-DD HH:mm:ss');
+
   res.status(200).json({
     message: "Sesión activa",
-    session: sessions[sessionId],
+    session:
+      req.session.user,
+      lastAccessedAt: lastAccessedAtFormatted,
   });
 });
